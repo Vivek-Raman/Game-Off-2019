cea0d499a5f9c56700a22c964a271b6602a267bb
Vivek Raman <vr.ac4bf@live.com>
2019-11-02 18:38:57 +0530

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public bool canMove = true;
+    public bool canJump = true;
+    public float movementSpeed = 5f;
+    public float gravity = -1f;
+
+    private float _inH;
+    private float _inV;
+    private CharacterController _controller = null;
+
+    private void Awake()
+    {
+        this.TryGetComponent(out _controller);
+    }
+
+    private void Update()
+    {
+        _inH = Input.GetAxis("Horizontal");
+        _inV = Input.GetAxis("Vertical");
+        
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
+        if (canMove)
+        {
+            _controller.Move(_inH * 60f * Time.deltaTime * movementSpeed * Vector3.right);
+        }
+    }
+
+    private void Jump()
+    {
+        if (_controller.isGrounded && canJump == false)
+        {
+            canJump = true;
+        }
+    }
+}
